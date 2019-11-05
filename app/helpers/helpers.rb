6fe7f97b0a58b45ad60aeb743a7116b08e83d9d1
Hanna Lee <mani21na@gmail.com>
2019-11-05 01:29:27 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+  #These two methods will only ever be called in views in order to add double protection to this view so that only the current user, when they are logged in, can see their bank account balance. Helpers are methods that make it cleaner to add logic to our views.
+  
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
+  end 
+  
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
+  end
 end
---
+++ db/migrate/20191104224433_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :Users do |x|
+      x.string :username 
+      x.string :password 
+      x.integer :balance
+    end
+  end
+end
