8530bd5ac9686c894de9fa1b87e0d48d5ad7da55
Hanna Lee <mani21na@gmail.com>
2019-11-05 01:33:21 +0000

    Done.

--- db/migrate/20191104224433_create_users.rb
+++ db/migrate/20191104224433_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :Users do |x|
       x.string :username 
       x.string :password 
-      x.integer :balance
+      x.decimal :balance
     end
   end
 end
